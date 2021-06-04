6effc94853ee6e8937824126e6a0bee5fa26999f
Derek-Mactal <dejumactal@gmail.com>
2021-06-04 01:40:57 -0700

    Added Feb10 MorningAlgos in python

---
+++ Python/Feb10_MorningAlgo.js
@@ -0,0 +1,19 @@
+//Compare strings case insensitively. 
+//declare both strings 
+//modify each string with either 'toLowerCase()' or 'toUpperCase' which will make the case the same for both strings 
+
+const strA1 = "ABC";
+const strB1 = "abc";
+const expected1 = true;
+
+const strA2 = "ABC";
+const strB2 = "abd";
+const expected2 = false;
+
+const strA3 = "ABC";
+const strB3 = "bc";
+const expected3 = false;
+
+function caseInsensitiveStringCompare(strA1, strB){
+
+}
